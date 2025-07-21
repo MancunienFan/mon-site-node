dee8c6d60139f7d40a53d39a3f809e148daa103a
ZAKARIA DJELLAL <zakaria.djellal@racj.gouv.qc.ca>
2025-07-21 12:05:19 -0400

    ajout du logo et dtm

--- oracle/clientDB.js
+++ oracle/clientDB.js
@@ -10,17 +10,17 @@ async function getConnection(session) {
 
 
 async function enregistrerPhotoDansCOUTCLI(id, base64Image, session) {
-  
-   let connection;
-  if (!session.user || !session.user.username || !session.user.password) {
-  throw new Error("Session invalide : informations utilisateur manquantes");
-}
 
-connection = await oracledb.getConnection({
-  user: session.user.username,
-  password: session.user.password,
-  connectString: '10.102.109.105:1521/acc1'
-});
+  let connection;
+  if (!session.user || !session.user.username || !session.user.password) {
+    throw new Error("Session invalide : informations utilisateur manquantes");
+  }
+
+  connection = await oracledb.getConnection({
+    user: session.user.username,
+    password: session.user.password,
+    connectString: '10.102.109.105:1521/acc1'
+  });
 
   if (!connection) throw new Error("Connexion Oracle échouée");
 
@@ -28,19 +28,21 @@ connection = await oracledb.getConnection({
   const imageBuffer = Buffer.from(base64Data, 'base64');
 
   try {
-await connection.execute(
-  `UPDATE COUT_CLI
+    await connection.execute(
+      `UPDATE COUT_CLI
    SET PHOTO = :photo,
        DT_PHOTO_PRISE = SYSDATE,
-       CO_EMPLO = :co_employe
+       CO_EMPLO = :co_employe,
+       DTM = SYSDATE
+
    WHERE NO_DOSS = :id`,
-  {
-    photo: { val: imageBuffer, type: oracledb.BLOB },
-    id: parseInt(id),
-    co_employe: session.user.username
-  },
-  { autoCommit: true }
-);
+      {
+        photo: { val: imageBuffer, type: oracledb.BLOB },
+        id: parseInt(id),
+        co_employe: session.user.username
+      },
+      { autoCommit: true }
+    );
 
     console.log("✅ Photo insérée");
   } catch (err) {
@@ -56,14 +58,14 @@ async function getNomPrenomClient(id, session) {
   let connection;
 
   if (!session.user || !session.user.username || !session.user.password) {
-  throw new Error("Session invalide : informations utilisateur manquantes");
-}
+    throw new Error("Session invalide : informations utilisateur manquantes");
+  }
 
-connection = await oracledb.getConnection({
-  user: session.user.username,
-  password: session.user.password,
-  connectString: '10.102.109.105:1521/acc1'
-});
+  connection = await oracledb.getConnection({
+    user: session.user.username,
+    password: session.user.password,
+    connectString: '10.102.109.105:1521/acc1'
+  });
 
   const result = await connection.execute(
     `SELECT NM_CLI, PN_CLI, PHOTO, DT_PHOTO_PRISE FROM COUT_CLI WHERE NO_DOSS = :id`,
